6579db81abe37e57e11d49ca5b695c7295070023
danielmachado <danielmachadopintos@gmail.com>
2023-12-20 22:54:29 -0300

    Remove unused code in TestConsumerEvent

--- src/consumer/test.consumer.event.ts
+++ src/consumer/test.consumer.event.ts
@@ -3,7 +3,6 @@ import { Job } from 'bullmq';
 
 @Processor('testQueue')
 export class TestConsumerEvent extends WorkerHost {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async process(job: Job<any, any, string>): Promise<any> {
     console.log('Início do processamento', job.data);
 
