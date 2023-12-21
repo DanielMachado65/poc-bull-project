39f92c48dfd747cf8e5842dfc7c92ce2ba1085a5
danielmachado <danielmachadopintos@gmail.com>
2023-12-20 22:54:12 -0300

    Refactor TestConsumerEvent class
    This commit refactors the TestConsumerEvent class by removing unused code and unnecessary console logs.

--- src/consumer/test.consumer.event.ts
+++ src/consumer/test.consumer.event.ts
@@ -3,27 +3,18 @@ import { Job } from 'bullmq';
 
 @Processor('testQueue')
 export class TestConsumerEvent extends WorkerHost {
-  private isProcessing = false;
-
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async process(job: Job<any, any, string>): Promise<any> {
-    if (this.isProcessing) {
-      console.log('Já existe um processamento ativo, ignorando este job');
-      return;
-    }
-
-    this.isProcessing = true;
     console.log('Início do processamento', job.data);
 
     // Espera por 30 segundos
     await new Promise((resolve) => setTimeout(resolve, 30000));
 
     console.log('Processamento concluído após 30 segundos');
-    this.isProcessing = false;
   }
 
   @OnWorkerEvent('completed')
   onCompleted() {
     console.log('completed');
-    this.isProcessing = false;
   }
 }
