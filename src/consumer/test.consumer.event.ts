e39479af1aa584593ae074081518d4aaf9545cc3
danielmachado <danielmachadopintos@gmail.com>
2023-12-20 22:50:12 -0300

    Update TestConsumerEvent to include logging and delay

--- src/consumer/test.consumer.event.ts
+++ src/consumer/test.consumer.event.ts
@@ -5,7 +5,12 @@ import { Job } from 'bullmq';
 export class TestConsumerEvent extends WorkerHost {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async process(job: Job<any, any, string>): Promise<any> {
-    console.log('processing job');
+    console.log('Início do processamento', job.data);
+
+    // Espera por 30 segundos
+    await new Promise((resolve) => setTimeout(resolve, 30000));
+
+    console.log('Processamento concluído após 30 segundos');
   }
 
   @OnWorkerEvent('completed')
